02dea28402bd6802015fe08e615311cf32840e7a
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-19 16:32:15 -0500

    Update sketch.js
    On click not working. Line 22 and 23 Mouse location needs tweaked

--- sketch.js
+++ sketch.js
@@ -18,20 +18,18 @@ function draw() {
   var c = map(mouseX, 0, width, 0, 255);
   fill(rectColor);
     
-  if(mouseIsPressed && (mouseX > 100) && (mouseX < 180) &&
-    (mouseY > 100) && (mouseY < 180))
-    {
-      for (var i = 0; i < x.length; i++){
-        fill(0);
-        rect(x[i],50,50,50);
+  for (var i = 0; i < x.length; i++){
+    if(mouseIsPressed && (mouseX > x[i]) && (mouseX < x[i]) &&
+    (mouseY > [i]+50) && (mouseY < [i]+50))
+      {
+      fill(0);
+      rect(x[i],50,50,50);
         
-    } 
-    }
-  else {
-      
-      for (var i = 0; i < x.length; i++){
-        fill(colors[i]);
-        rect(x[i],50,50,50);
-            }
-          }
+      } 
+ 
+    else {
+      fill(colors[i]);
+      rect(x[i],50,50,50);
+      }
+  }
 }
