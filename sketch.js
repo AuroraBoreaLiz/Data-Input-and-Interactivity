0fc238f4ece40f49a929459db9a21c3b7e91ee19
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-21 14:17:37 -0500

    Update sketch.js
    Splitting out the functions. The click is still not working as intended within the rectangle constraints at the moment.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-var table;
-var rectColor = 150;
-
 function preload() {
   table = loadTable("names.csv", "csv", "header");
 }
+
+var table;
+var rectColor = 150;
+
 
 function setup() {
   createCanvas(400, 400);
@@ -12,29 +13,39 @@ function setup() {
 
 function draw() {
   background(220);
-  noStroke();
+  noStroke(); 
   var x = table.getColumn("x");
-  var y = 50
-  var boxX = 50
-  var boxY = 50
+  var y = 50;
+  var rectWidth = 50;
+  var rectHeight = 50;
+  var rectRadius = 5;
   var colors = table.getColumn("color");
   var c = map(mouseX, 0, width, 0, 255);
-  fill(rectColor);
   
   text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
-    
+  //print(mouseX < (x[0]+25));
+  //print(mouseX);
+  //print(x[0]);
   for (var i = 0; i < x.length; i++){
-    if(mouseIsPressed && (mouseX > (x[i]-25)) && (mouseX < (x[i]+25)) && (mouseY > (boxY-25)) && (mouseY < (boxY+25))){
-      fill(0);
-      rect(x[i],y,boxX,boxY);
-        
-      } 
- 
-    else {
-      fill(colors[i]);
-      rect(x[i],y,boxX,boxY);
-      }
-  }
-}      
+      rectColor = colors[i];
+      fill(rectColor);
+      rect(x[i],y,rectWidth,rectHeight,rectRadius, rectRadius, rectRadius, rectRadius);
+      mouseFill(x,y,rectWidth,rectHeight,rectRadius,colors,c);
+  }    
+    
+}
 
 
+function mouseFill(x,y,rectWidth,rectHeight,rectRadius,colors,c){
+
+  for (var i = 0; i < x.length; i++){
+      if(mouseIsPressed && 
+         (mouseX >= (x[i]-25)) && (mouseX <= (x[i]+25)) && 
+         (mouseY >= (25)) && (mouseY <= (75)))
+      {
+        fill(0);
+        rect(x[i],y,rectWidth,rectHeight,rectRadius, rectRadius, rectRadius, rectRadius);
+
+    }
+  }     
+}
