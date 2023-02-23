efd40a10ddf4b65f40dde471bd2ed3f8f2d024f2
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-23 16:39:08 -0500

    Update sketch.js
    Three cubes in a for loop with a separate function mouse click. Needed to be in different number arrays. Raven Helped.

--- sketch.js
+++ sketch.js
@@ -1,33 +1,88 @@
-var rectColor = 150;
-var rectColor2 = 55;
-var rectColor3 = 25;
+var rectColor = [150, 55, 25];
+var rectSize = [50, 150, 250];
+var fillColor = [];
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(400, 400);
   background(200);
 }
 
 function draw() {
+  for (var i = 0; i < rectSize.length; i++) {
+    push();
+    print(fillColor,"filling",i)
+    x = rectSize[i];
+    mouseFill(x);
+    
+    fill(fillColor[i]);
+    rect(x, x, 80, 80);
+    pop();
+    
+    
+  }
   
-  fill(rectColor);
-  rect(50,50, 80,80);
-  
-  fill(rectColor2);
-  rect(150,150,80,80);
-  
-  fill(rectColor3);
-  rect(250,250,80,80);
-  
-  
+}
+
+function mouseFill(x) {
+  if (
+    mouseIsPressed &&
+    mouseX > 50 &&
+    mouseX < 130 &&
+    mouseY > 50 &&
+    mouseY < 130
+  ) {
+    push();
+    print("First Box")
+    fillColor[0] = 255;
+    //rect(x, x, 80, 80);
+    push();
+  } else {
+    fillColor[0] = rectColor[0];
+  }
+
+  if (
+    mouseIsPressed &&
+    mouseX > 150 &&
+    mouseX < 230 &&
+    mouseY > 150 &&
+    mouseY < 230
+  ) {
+    push();
+    print("Second Box")
+    fillColor[1] = 255;
+    //rect(x, x, 80, 80);
+    push();
+  } else {
+    fillColor[1] = rectColor[1];
+  }
+
+  if (
+    mouseIsPressed &&
+    mouseX > 250 &&
+    mouseX < 330 &&
+    mouseY > 250 &&
+    mouseY < 330
+  ) {
+    push();
+    print("Third Box",fillColor)
+    fillColor[2] = 255;
+    //rect(x, x, 80, 80);
+    push();
+  } else {
+  fillColor[2] = rectColor[2];
+  }
+}
+/*
+function mouseFill(rectColor){
   if(mouseIsPressed &&
      (mouseX > 50) && (mouseX < 130) &&
      (mouseY > 50) && (mouseY < 130)
     )
   {
-    rectColor = 0;
+    fillColor = 0;
   } 
   else {
-    rectColor = 150;
+    fillColor = 150;
   }
 
     
@@ -38,10 +93,10 @@ function draw() {
     
   
   {
-    rectColor2 = 55;
+    fillColor = 55;
   } 
   else {
-    rectColor2 = 150;
+    fillColor = 150;
   }
   
     if(mouseIsPressed &&
@@ -51,11 +106,10 @@ function draw() {
     
   
   {
-    rectColor3 = 255;
+    fillColor = 255;
   } 
   else {
-    rectColor3 = 150;
+    fillColor = 150;
   }
   
-
-}
+*/
