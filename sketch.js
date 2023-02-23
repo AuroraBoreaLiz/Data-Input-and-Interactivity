4d79ae49509f390ce3aa086ff1196be650ef94c1
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-23 14:20:07 -0500

    Update sketch.js
    Starting Over. Three squares that are clickable.

--- sketch.js
+++ sketch.js
@@ -1,49 +1,61 @@
-function preload() {
-  table = loadTable("names.csv", "csv", "header");
-}
-
-var table;
 var rectColor = 150;
-
+var rectColor2 = 55;
+var rectColor3 = 25;
 
 function setup() {
-  createCanvas(400, 400);
-  rectMode(CENTER);
+  createCanvas(400,400);
+  background(200);
 }
 
 function draw() {
-  background(220);
-  noStroke(); 
-  var x = table.getColumn("x");
-  var y = 50;
-  var rectWidth = 50;
-  var rectHeight = 50;
-  var rectRadius = 5;
-  var colors = table.getColumn("color");
-  var c = map(mouseX, 0, width, 0, 255);
   
-  text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
-  //print(mouseX < (x[0]+25));
-  //print(mouseX);
-  //print(x[0]);
-  for (var i = 0; i < x.length; i++){
-      rectColor = colors[i];
-      fill(rectColor);
-      rect(x[i],y,rectWidth,rectHeight,rectRadius, rectRadius, rectRadius, rectRadius);
-      mouseFill(x,y,i,rectWidth,rectHeight,rectRadius,colors,c);
-  }    
+  fill(rectColor);
+  rect(50,50, 80,80);
+  
+  fill(rectColor2);
+  rect(150,150,80,80);
+  
+  fill(rectColor3);
+  rect(250,250,80,80);
+  
+  
+  if(mouseIsPressed &&
+     (mouseX > 50) && (mouseX < 130) &&
+     (mouseY > 50) && (mouseY < 130)
+    )
+  {
+    rectColor = 0;
+  } 
+  else {
+    rectColor = 150;
+  }
+
     
-}
-
-
-function mouseFill(x,y,i,rectWidth,rectHeight,rectRadius,colors,c){
-      if(mouseIsPressed && 
-         (mouseX >= (x[i]-25)) && (mouseX <= (x[i]+25)) && 
-         (mouseY >= (25)) && (mouseY <= (75)))
-      {
-        fill(0);
-        rect(x[i],y,rectWidth,rectHeight,rectRadius, rectRadius, rectRadius, rectRadius);
-
-    }
+  if(mouseIsPressed &&
+     (mouseX > 150) && (mouseX < 230) &&
+     (mouseY > 150) && (mouseY < 230)
+    )
     
+  
+  {
+    rectColor2 = 55;
+  } 
+  else {
+    rectColor2 = 150;
+  }
+  
+    if(mouseIsPressed &&
+     (mouseX > 250) && (mouseX < 330) &&
+     (mouseY > 250) && (mouseY < 330)
+    )
+    
+  
+  {
+    rectColor3 = 255;
+  } 
+  else {
+    rectColor3 = 150;
+  }
+  
+
 }
