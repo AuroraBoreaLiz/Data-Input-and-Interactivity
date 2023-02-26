fc829233e10e1d4edab4797cdcca13d5c8582ecf
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-26 00:23:48 -0500

    Update sketch.js
    condensing the if statement for the function mouseFill to work with the variable.

--- sketch.js
+++ sketch.js
@@ -10,10 +10,9 @@ function setup() {
 function draw() {
   for (var i = 0; i < rectSize.length; i++) {
     push();
-      //print(fillColor,"filling",i)
       x = rectSize[i];
       mouseFill(x);
-      fill(fillColor[i]);
+      fill(fillColor);
       rect(x, x, 80, 80);
     pop();
     
@@ -25,90 +24,15 @@ function draw() {
 function mouseFill(x) {
   if (
     mouseIsPressed &&
-    mouseX > 50 &&
-    mouseX < 130 &&
-    mouseY > 50 &&
-    mouseY < 130
+    mouseX > x &&
+    mouseX < x+80 &&
+    mouseY > x &&
+    mouseY < x+80 
   ) {
     push();
-    print("First Box")
-      fillColor[0] = 255;
-      //rect(x, x, 80, 80);
+      fillColor = 255;
     push();
   } else {
-    fillColor[0] = rectColor[0];
-  }
-
-  if (
-    mouseIsPressed &&
-    mouseX > 150 &&
-    mouseX < 230 &&
-    mouseY > 150 &&
-    mouseY < 230
-  ) {
-    push();
-      print("Second Box")
-      fillColor[1] = 255;
-      //rect(x, x, 80, 80);
-    push();
-  } else {
-    fillColor[1] = rectColor[1];
-  }
-
-  if (
-    mouseIsPressed &&
-    mouseX > 250 &&
-    mouseX < 330 &&
-    mouseY > 250 &&
-    mouseY < 330
-  ) {
-    push();
-      print("Third Box")
-      fillColor[2] = 255;
-      //rect(x, x, 80, 80);
-    push();
-  } else {
-    fillColor[2] = rectColor[2];
+    fillColor= rectColor;
   }
 }
-/*
-function mouseFill(rectColor){
-  if(mouseIsPressed &&
-     (mouseX > 50) && (mouseX < 130) &&
-     (mouseY > 50) && (mouseY < 130)
-    )
-  {
-    fillColor = 0;
-  } 
-  else {
-    fillColor = 150;
-  }
-
-    
-  if(mouseIsPressed &&
-     (mouseX > 150) && (mouseX < 230) &&
-     (mouseY > 150) && (mouseY < 230)
-    )
-    
-  
-  {
-    fillColor = 55;
-  } 
-  else {
-    fillColor = 150;
-  }
-  
-    if(mouseIsPressed &&
-     (mouseX > 250) && (mouseX < 330) &&
-     (mouseY > 250) && (mouseY < 330)
-    )
-    
-  
-  {
-    fillColor = 255;
-  } 
-  else {
-    fillColor = 150;
-  }
-  
-*/
