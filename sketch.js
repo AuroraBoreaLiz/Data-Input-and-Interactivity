8c7c4f72106ea14bab3357345a763ceef06ae361
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-26 12:58:00 -0500

    Update sketch.js
    Stars now have visible color and variance once three numbers were added to the fill color.

--- sketch.js
+++ sketch.js
@@ -4,29 +4,14 @@ function preload() {
 
 var table;
 var y = [];
-n = 30;
-
-function setup() {
-  createCanvas(400, 400);
-  background(0,0,35);
-  for(var i=0; i<n; i++) {
-    y[i] = random(height);
-  }
-  
-}
-
-function draw() {function preload() {
-  table = loadTable("stars.csv", "csv", "header");
-}
-
-var table;
-var y = [];
 var x = [];
 n = 30;
 
 function setup() {
   createCanvas(400, 400);
   
+  //from class example
+  //fill up tables with random numbers
   for(var i=0; i<n; i++) {
     y[i] = random(height);
     x[i] = random(width);
@@ -36,17 +21,18 @@ function setup() {
 
 function draw() {
   background(0,0,35);
-  var color = table.getColumn("starColor");
+  var color = table.getColumn("starColor1");
   
   for (var i = 0; i < color.length; i++) { 
     
     push();
-      starColor = color[i];
+      starColor1 = color[i];
       translate(x[i],y[i]);
-      fill(starColor);
+      fill(starColor1,0,100);
       star(0, 0, 10, 7, 5);
     pop();
     
+    //code from class example
     if(mouseIsPressed) {
       if(!dragging && 
          dist(mouseX, mouseY, x[i], y[i]) < 8) {  
@@ -71,33 +57,7 @@ function draw() {
   
 }
 
-function star(x, y, radius1, radius2, npoints) {
-  let angle = TWO_PI / npoints;
-  let halfAngle = angle / 2.0;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius2;
-    let sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
-}
-
-  var starMap1 = table.getColumn("starLocation1");
-  
-  for (var i = 0; i < starMap1.length; i++) { 
-    starTranslate1 = starMap1[i];
-    push();
-      translate(starTranslate1,y[i]);
-      star(0, 0, 10, 8, 6);
-    pop();
-  }
-  
-}
-
+//code from p5.js star example: https://p5js.org/examples/form-star.html
 function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
