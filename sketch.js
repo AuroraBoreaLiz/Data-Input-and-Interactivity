509e6aee506ad347149399ed2c43a562b6cfa326
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-26 14:37:19 -0500

    Update sketch.js
    stars and moons working.

--- sketch.js
+++ sketch.js
@@ -5,32 +5,57 @@ function preload() {
 var table;
 var y = [];
 var x = [];
+var my = [];
+var mx = [];
 n = 30;
 
 function setup() {
   createCanvas(400, 400);
   
   //from class example
-  //fill up tables with random numbers
+  //fill up star x,y tables with random numbers
   for(var i=0; i<n; i++) {
     y[i] = random(height);
     x[i] = random(width);
   }
-  
+  //fillup the moon x,y tables
+  for(var m=0; m<n; m++) {
+    my[m] = random(height);
+    mx[m] = random(width);
+  }
 }
 
 function draw() {
-  background(0,0,35);
+  background(190,166,222);
   var color = table.getColumn("starColor1");
+  var mp1 = table.getColumn("moonPosition1");
+  var mp2 = table.getColumn("moonPosition2");
   
   for (var i = 0; i < color.length; i++) { 
     
     push();
-      starColor1 = color[i];
+      mx1 = mp1[i];
+      my2 = mp2[i];
+      noStroke();
+      translate(mx[i],my[i]);
+      fill(230,230,180);
+      ellipse(0,0,25,25);
+    
+      noStroke();
+      fill(190,166,222);
+      ellipse(mx1,my2,20,20);
+    pop();
+    
+    push();
+      noStroke();
+      var starColor1 = color[i];
       translate(x[i],y[i]);
       fill(starColor1,0,100);
       star(0, 0, 10, 7, 5);
     pop();
+    
+    push();
+      
     
     //code from class example
     if(mouseIsPressed) {
@@ -43,6 +68,18 @@ function draw() {
       }
     
     } 
+  /*
+    else if(mouseIsPressed) {
+      if(!dragging && 
+         dist(mouseX, mouseY, mx[i], my[i]) < 8) {  
+        
+        dragging = true;  // start dragging a circle    
+        drag = i;  // drag this circle
+      
+      }
+    
+    } 
+    */
     else {
       dragging = false;  // mouse 
     }
